be829d42762f8ddaf06df47407304e7ac7568f42
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-17 23:32:07 +0600

    strrev

--- strrev.go
+++ strrev.go
@@ -3,7 +3,10 @@ package piscine
 func StrRev(s string) string {
 	runes := []rune(s)
 	var a rune
-	b := len(s)
+	b := 0 
+	for j := range runes {
+		b++
+	}
 	for i := 0; i < b/2; i++ {
 		a = runes[i]
 		runes[i] = runes[b-i-1]
