ac114312f866db5fc320b5f58e385b5772a55c05
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-20 17:16:45 +0600

    atoi

--- basicatoi.go
+++ basicatoi.go
@@ -3,8 +3,8 @@ package piscine
 func BasicAtoi(s string) int {
 	b := 0
 	for _, n := range s {
-		a := 0	
-		for  i := '1'; i <= n; i++ { 
+		a := 0
+		for i := '1'; i <= n; i++ {
 			a++
 		}
 		b = b*10 + a
