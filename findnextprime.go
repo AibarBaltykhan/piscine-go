9f55728ff11185c7291851ef6a07da1a5c4e70ed
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-21 20:43:23 +0600

    f

---
+++ findnextprime.go
@@ -0,0 +1,15 @@
+package piscine
+
+func FindNextPrime(nb int) int {
+	s := nb
+	if nb < 2 {
+		return 2
+	}
+	for i := 3; i <= nb/2; i++ {
+		if nb%i == 0 {
+			return indNextPrime(nb+1)
+		}
+	}
+	return s
+
+}
