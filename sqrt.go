559463370cb5e2e40bb7243a57640c276237e7e9
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-21 20:29:23 +0600

    fibonacci

--- sqrt.go
+++ sqrt.go
@@ -1,8 +1,8 @@
 package piscine
 
 func Sqrt(nb int) int {
-	for i := 2; i <= nb/2; i ++ {
-		if nb/i == i && nb % i == 0 {
+	for i := 2; i <= nb/2; i++ {
+		if nb/i == i && nb%i == 0 {
 			return i
 		}
 	}
