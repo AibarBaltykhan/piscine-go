9e5385eb877c4ec8c1b46f7c6ddacccdf04f6117
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-22 22:11:22 +0600

    ind

---
+++ index.go
@@ -0,0 +1,31 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	w1 := []rune(s)
+	w2 := []rune(toFind)
+	k := -1
+	l := -1
+	t := 0
+	for index, i := range w2 {
+		k++
+		i = i
+		index = index
+	}
+	for index, j := range w1 {
+		l++
+		j = j
+		inde = inde
+		if j == w2[0] {
+			for m := l; m <= l + k; l++ {
+				if w1[m] == w2[k] {
+					t++
+				}
+
+			}
+			if t == k + 1 {
+				return l
+			}
+		}
+	}
+	return -1
+}
