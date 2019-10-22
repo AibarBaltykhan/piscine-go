e7f1152e0c067994b72f036158cb7fa2ff68611a
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-22 21:31:40 +0600

    nr

--- nrune.go
+++ nrune.go
@@ -4,10 +4,10 @@ func NRune(s string, n int) rune {
 	runes := []rune(s)
 	s := 0
 	for index, i := range runes {
-			i = i
-			s = index
+		i = i
+		s = index
 	}
-	if (n-1) > s {
+	if (n - 1) > s {
 		return 0
 	}
 	return runes[n-1]
