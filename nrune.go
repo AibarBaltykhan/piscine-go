04d2a1b3f658f2fe3746603048339f8a660b249f
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-22 21:30:47 +0600

    nr

--- nrune.go
+++ nrune.go
@@ -2,5 +2,13 @@ package piscine
 
 func NRune(s string, n int) rune {
 	runes := []rune(s)
+	s := 0
+	for index, i := range runes {
+			i = i
+			s = index
+	}
+	if (n-1) > s {
+		return 0
+	}
 	return runes[n-1]
 }
