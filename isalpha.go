bb51dbe086746ece34956207721e66902e404a0c
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-23 21:39:17 +0600

    tu

--- isalpha.go
+++ isalpha.go
@@ -5,6 +5,7 @@ func IsAlpha(str string) bool {
     for i := range r {
         if (runes[i-1] >= 33 && runes[i-1] <= 47) || (runes[i-1] >= 58 && runes[i-1] <= 64) || (runes[i-1] >= 91 && runes[i-1] <= 96) || (runes[i-1] >= 123 && runes[i-1] <= 126) || runes[i-1] == ' ' {
             return false
+        }
     }
     return true
 }
