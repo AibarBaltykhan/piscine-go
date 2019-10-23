e2f2272df6427a204b38cf5458a8ed420443f569
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-23 22:38:15 +0600

    tu

---
+++ printprogramname/main.go
@@ -0,0 +1,15 @@
+package piscine
+
+import ( 
+		"github.com/01-edu/z01.PrintRune"
+		"os"
+)
+
+func main() {
+	a := os.Args
+	name := []rune(a[0])
+	for i := range name {
+		z01.PrintRune(name[i])
+	}
+	z01.PrintRune(10)
+}
