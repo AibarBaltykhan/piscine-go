50ea32aa10c0547dd74462babe08d784313cfc6a
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-23 20:34:12 +0600

    tu

--- capitalize.go
+++ capitalize.go
@@ -9,10 +9,18 @@ func Capitalize(s string) string {
 		y = y
 	}
 	for i := 0; i <= l; i++ {
-		if runes[i] == ' ' {
+		if runes[0] >= 'a' && runes[0] <= 'z' {
+			runes[0] = runes[0] - 32
+		}
+		if (runes[i] < 'a' || runes[i] > 'z') && (runes[i] < 'A' || runes[i] > 'Z') {
 			if runes[i+1] >= 'a' && runes[i+1] <= 'z' {
 				runes[i+1] = runes[i+1] - 32
 			}
+		} else {
+			if runes[i+1] >= 'A' && runes[i+1] <= 'Z' {
+				runes[i+1] = runes[i+1] + 32
+			} 
+
 		}
 	}
 	s = string(runes)
