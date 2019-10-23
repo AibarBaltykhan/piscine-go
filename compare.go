aff161711a1487ba233fd3d9c51f80245a39de57
AibarBaltykhan <52800030+AibarBaltykhan@users.noreply.github.com>
2019-10-23 12:02:58 +0600

    Update compare.go

--- compare.go
+++ compare.go
@@ -1,9 +1,9 @@
 package piscine
 
 func Compare(a, b string) int {
-  l1 := -1
+	l1 := -1
 	l2 := -1
-  n := 0
+ 	n := 0
 	for a, y := range a {
 		l1++
 		a = a
@@ -14,28 +14,28 @@ func Compare(a, b string) int {
 		b = b
 		z = z
 	}
-  if l1 > l2 {
-    n = l2
-  } else { 
-    n = l1
-  }
+  	if l1 > l2 {
+    	n = l2
+  	} else { 
+    		n = l1
+ 	}
   	r1 := []rune(a)
-		r2 := []rune(b)
-  for i := 0; i <= l1; i++ {
-    if r1[i] > r2[i] {
-      return 1
-    }
-    if r1[i] < r2[i] {
-      return -1
-    }
-  }
-  if l1 == l2 {
-    return 0
-  }
-  if l1 > l2 {
-    return 1
-    }
-  if l1 < l2 {
-    return -1
-    }
+	r2 := []rune(b)
+  	for i := 0; i <= l1; i++ {
+    		if r1[i] > r2[i] {
+      			return 1
+    		}
+    		if r1[i] < r2[i] {
+      			return -1
+    		}
+  	}
+  	if l1 == l2 {
+    	return 0
+  	}
+  	if l1 > l2 {
+    		return 1
+    	}
+  	if l1 < l2 {
+    	return -1
+    	}
 }
