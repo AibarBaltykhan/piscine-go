d5be691c06732c2b2d9e2d54dc6b0137c286f296
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-23 21:08:14 +0600

    tu

--- capitalize.go
+++ capitalize.go
@@ -8,21 +8,18 @@ func Capitalize(s string) string {
 		a = a
 		y = y
 	}
-	for i := 0; i <= l; i++ {
+	for i := l; i <= 0; i-- {
 		if runes[0] >= 'a' && runes[0] <= 'z' {
 			runes[0] = runes[0] - 32
 		}
-		if i < l {
-			if (runes[i] < 'a' || runes[i] > 'z') && (runes[i] < 'A' || runes[i] > 'Z') {
-				if runes[i+1] >= 'a' && runes[i+1] <= 'z' {
-					runes[i+1] = runes[i+1] - 32
-				}
-			} else {
-				if (runes[i] >= 'A' && runes[i] <= 'Z') || (runes[i] >= '0' && runes[i] <= '9') {
-					if runes[i+1] >= 'A' && runes[i+1] <= 'Z' {
-						runes[i+1] = runes[i+1] + 32
-					}
-				}
+		if (runes[i-1] >= 33 && runes[i-1] <= 47) || (runes[i-1] >= 58 && runes[i-1] <= 64) || (runes[i-1] >= 91 && runes[i-1] <= 96) || (runes[i-1] >= 123 && runes[i-1] <= 126) {
+			if runes[i] >= 'a' && runes[i] <='z' {
+				runes[i] = runes[i] + 32
+			}
+		}
+		if (runes[i-1] >= 48 && runes[i-1] <= 57) || (runes[i-1] >= 65 && runes[i-1] <= 90) || (runes[i-1] >= 97 && runes[i-1] <= 122) {
+			if runes[i] >= 'A' && runes[i] <='Z' {
+				runes[i] = runes[i] - 32
 			}
 		}
 	}
