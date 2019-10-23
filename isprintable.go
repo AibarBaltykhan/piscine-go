08ceafdf4969db901e10e86ab34aecf0480810db
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-23 22:10:54 +0600

    tu

--- isprintable.go
+++ isprintable.go
@@ -3,7 +3,7 @@ package piscine
 func IsPrintable(str string) bool {
 	runes := []rune(str)
 	for i := range runes {
-		if runes[i] < 33 || runes[i] > 126 {
+		if runes[i] < 32 || runes[i] > 126 {
 			return false
 		}
 	}
