86aea3c395cca2f18e6ab5a6f4bf89d270fe3360
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-23 21:45:02 +0600

    tu

--- isalpha.go
+++ isalpha.go
@@ -1,11 +1,11 @@
 package piscine
 
 func IsAlpha(str string) bool {
-    r := []rune(str)
-    for i := range r {
-        if (runes[i-1] >= 33 && runes[i-1] <= 47) || (runes[i-1] >= 58 && runes[i-1] <= 64) || (runes[i-1] >= 91 && runes[i-1] <= 96) || (runes[i-1] >= 123 && runes[i-1] <= 126) || runes[i-1] == ' ' {
-            return false
-        }
-    }
-    return true
+	r := []rune(str)
+	for i := range r {
+		if (runes[i-1] >= 33 && runes[i-1] <= 47) || (runes[i-1] >= 58 && runes[i-1] <= 64) || (runes[i-1] >= 91 && runes[i-1] <= 96) || (runes[i-1] >= 123 && runes[i-1] <= 126) || runes[i-1] == ' ' {
+			return false
+		}
+	}
+	return true
 }
