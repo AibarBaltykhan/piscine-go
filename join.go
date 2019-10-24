6b8ee4302d035198f9e11771b2c34658c5fa774f
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-24 22:25:34 +0600

    tu

---
+++ join.go
@@ -0,0 +1,15 @@
+package piscine
+
+func Join(strs []string, sep string) string {
+	var s string
+	k := 0
+	for i := range strs {
+		i = i
+		k++
+	}
+	for j := 0; j < k-1; j++ {
+		s = s + strs[i] + sep
+	}
+	s = s + strs[k-1]
+	return s
+}
