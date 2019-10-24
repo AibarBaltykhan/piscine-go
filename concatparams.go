04c6802013df8a4879a14ab47ed66423692e8df9
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-24 21:15:33 +0600

    tu

--- concatparams.go
+++ concatparams.go
@@ -2,8 +2,17 @@ package piscine
 
 func ConcatParams(args []string) string {
 	var a string
+	l := -1
 	for i := range args {
-		a = a + args[i] + "\n"
+		i = i
+		l++
+	}
+	for i := range args {
+		if i <= l-2 {
+			a = a + args[i] + "\n"
+		} else {
+			a = a + args[i]
+		}
 	}
 	return a
 }
