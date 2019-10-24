ea150499542fcfd507a8a8775fde5224a69cefe1
AibarBaltykhan <AibarBaltykhan@github.com>
2019-10-24 21:57:09 +0600

    tu

---
+++ printwordstables.go
@@ -0,0 +1,41 @@
+package piscine
+
+func SplitWhiteSpaces(str string) []string {
+	len := 0
+	strlLen := 0
+	start := 0
+	j := 0
+	b := false
+	for _, v := range str {
+		if (v == '\n' || v == '\t' || v == ' ') && b {
+			len++
+			b = false
+		} else if v != '\n' && v != '\t' && v != ' ' {
+			b = true
+		}
+		strlLen++
+	}
+	if b {
+		len++
+		b = false
+	}
+	arr := make([]string, len)
+	for i, v := range str {
+		if (v == '\n' || v == '\t' || v == ' ') && b {
+			arr[j] = str[start:i]
+			j++
+			b = false
+		} else if v != '\n' && v != '\t' && v != ' ' {
+			if !b {
+				start = i
+			}
+			b = true
+		}
+	}
+	if b {
+		arr[j] = str[start:strlLen]
+	}
+	for k := 0; k <= len; k ++ {
+		PrintStr(arr[i] string)
+	}
+}
